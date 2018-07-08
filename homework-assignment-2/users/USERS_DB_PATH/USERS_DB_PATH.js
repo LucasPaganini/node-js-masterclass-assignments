3105196936305d4a530af795908d0e4856ab9085
Lucas Paganini de Moura <lucaspaganinidm@gmail.com>
2018-07-08 14:24:34 -0300

    add a const with the absolute path to the users database

---
+++ homework-assignment-2/users/USERS_DB_PATH/USERS_DB_PATH.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = require("path");
+exports.USERS_DB_PATH = path.join(__dirname, '../.usersdb');
---
+++ homework-assignment-2/users/USERS_DB_PATH/USERS_DB_PATH.sandbox.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const USERS_DB_PATH_1 = require("./USERS_DB_PATH");
+console.log(USERS_DB_PATH_1.USERS_DB_PATH);
