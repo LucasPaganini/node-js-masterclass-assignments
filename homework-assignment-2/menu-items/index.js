5987d0f04b686568473a88e7cf2dfd5a7bbd61af
Lucas Paganini de Moura <lucaspaganinidm@gmail.com>
2018-07-11 10:53:41 -0300

    add ids to menu items

--- homework-assignment-2/menu-items/index.js
+++ homework-assignment-2/menu-items/index.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const menuItems = [
     {
+        id: '1',
         title: 'Beef',
         price: 50,
     },
     {
+        id: '2',
         title: 'Salad',
         price: 30,
     },
--- homework-assignment-2/menu-items/index.ts
+++ homework-assignment-2/menu-items/index.ts
@@ -1,14 +1,17 @@
 export interface MenuItem {
+  id: string
   title: string
   price: number
 }
 
 const menuItems: Array<MenuItem> = [
   {
+    id: '1',
     title: 'Beef',
     price: 50,
   },
   {
+    id: '2',
     title: 'Salad',
     price: 30,
   },
