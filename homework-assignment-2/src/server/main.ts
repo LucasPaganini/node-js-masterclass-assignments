aa53248070e4f234f21b3c222dbcccbf32931b6c
Lucas Paganini de Moura <lucaspaganinidm@gmail.com>
2018-11-03 01:30:22 -0300

    bug fix: has matching wrong handlers

--- homework-assignment-2/src/server/main.ts
+++ homework-assignment-2/src/server/main.ts
@@ -3,7 +3,7 @@ import * as url from 'url'
 import * as handlers from './handlers'
 
 const server = http.createServer((request, response) => {
-  const path = url.parse(request.url, true).path
+  const path = url.parse(request.url, true).pathname
   const method = request.method
 
   console.log({ path, method })
@@ -14,7 +14,7 @@ const server = http.createServer((request, response) => {
       method === h.method &&
       // Route is string and it's equal or is RegExp and matches
       ((typeof h.route === 'string' && path === h.route) ||
-        (h.route as RegExp).test(path)),
+        (h.route instanceof RegExp && h.route.test(path))),
   )
 
   if (handler) handler.handle(request, response)
