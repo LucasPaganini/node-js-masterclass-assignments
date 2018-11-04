ffb4cde760e96f04748f770d955f3dfea78a4b0f
Lucas Paganini de Moura <lucaspaganinidm@gmail.com>
2018-11-04 13:56:45 -0200

    add handler to update the user data

--- homework-assignment-2/src/api/users/updateUser/updateUser.ts
+++ homework-assignment-2/src/api/users/updateUser/updateUser.ts
@@ -3,12 +3,23 @@ import { User } from '../User'
 import { USERS_DB_PATH } from '../USERS_DB_PATH'
 import { getUser } from '../getUser'
 import { writeFile } from 'fs'
+import { Omit } from '../../utils'
+import { hashPassword } from '../hashPassword'
+
+type Data = Partial<Omit<UserData, 'hashedPassword'> & { password: string }>
 
 export const updateUser = async (
   userID: string,
-  updatedData: Partial<UserData>,
+  updatedData: Data,
 ): Promise<User> => {
   const user = await getUser(userID)
+
+  if (updatedData.password) {
+    const hashedPassword = await hashPassword(updatedData.password)
+    ;(updatedData as UserData).hashedPassword = hashedPassword
+    delete updatedData.password
+  }
+
   const updatedUser = { ...user, ...updatedData }
   const fileData = JSON.stringify(updatedUser)
   const fullPath = `${USERS_DB_PATH}/${userID}.json`
