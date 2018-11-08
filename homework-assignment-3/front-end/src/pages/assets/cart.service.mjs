74b3b7f7f27c9ee6302df2a2e5ae75d10e14231f
Lucas Paganini de Moura <lucaspaganinidm@gmail.com>
2018-11-08 21:01:07 -0200

    add /cart

--- homework-assignment-3/front-end/src/pages/assets/cart.service.mjs
+++ homework-assignment-3/front-end/src/pages/assets/cart.service.mjs
@@ -34,4 +34,53 @@ export class CartService {
 
     return json.data.cart
   }
+
+  /**
+   * Removes one menu item from cart and returns the updated cart.
+   *
+   * @param {string} menuItemID ID of the menu item to be removed from cart
+   * @returns {Promise<Cart>} Updated cart
+   */
+  async removeItem(menuItemID) {
+    const data = { menuItemID }
+
+    const authHeaders = await this._auth.getAuthHeaders()
+    authHeaders.append('Content-Type', 'application/json')
+
+    const res = await fetch(API_HOST + '/cart', {
+      method: 'DELETE',
+      headers: authHeaders,
+      body: JSON.stringify(data),
+    })
+    const json = await res.json()
+
+    if (json.errors) {
+      const msg = json.errors.map(error => error.message).join(' ')
+      throw new Error(msg)
+    }
+
+    return json.data.cart
+  }
+
+  /**
+   * Gets the user cart.
+   *
+   * @returns {Promise<Cart>} Cart
+   */
+  async getCart() {
+    const authHeaders = await this._auth.getAuthHeaders()
+
+    const res = await fetch(API_HOST + '/cart', {
+      method: 'GET',
+      headers: authHeaders,
+    })
+    const json = await res.json()
+
+    if (json.errors) {
+      const msg = json.errors.map(error => error.message).join(' ')
+      throw new Error(msg)
+    }
+
+    return json.data.cart
+  }
 }
---
+++ homework-assignment-3/front-end/src/pages/cart/main.mjs
@@ -0,0 +1,77 @@
+import { AuthService } from '/assets/auth.service.mjs'
+import { MenuService } from '/assets/menu.service.mjs'
+import { CartService } from '/assets/cart.service.mjs'
+
+const auth = new AuthService()
+const cartService = new CartService(auth)
+const orderContainer = document.getElementById('order-container')
+const orderBody = orderContainer.querySelector('tbody')
+const totalDataCell = orderContainer.querySelector('#table-total-price')
+
+const logoutBtn = document.getElementById('logout-btn')
+
+logoutBtn.addEventListener('click', async () => {
+  const r = await auth.logout()
+})
+
+const renderCartItem = cartItem => {
+  // <td>{{ cartItem.title }}</td>
+  const title = document.createElement('td')
+  title.textContent = cartItem.title
+
+  /**
+   * <td>
+   *  <span>{{ cartItem.ammount }}</span>
+   *  <button>Add</button>
+   *  <button>Remove</button>
+   * </td>
+   */
+  const ammount = document.createElement('span')
+  ammount.textContent = cartItem.quantity
+
+  const addAmmount = document.createElement('button')
+  addAmmount.textContent = 'Add'
+  addAmmount.addEventListener('click', async () => {
+    await cartService.addItem(cartItem.id)
+    setTimeout(() => location.reload(), 250)
+  })
+
+  const removeAmmount = document.createElement('button')
+  removeAmmount.textContent = 'Remove'
+  removeAmmount.addEventListener('click', async () => {
+    await cartService.removeItem(cartItem.id)
+    setTimeout(() => location.reload(), 250)
+  })
+
+  const ammountContainer = document.createElement('td')
+  ammountContainer.appendChild(ammount)
+  ammountContainer.appendChild(addAmmount)
+  ammountContainer.appendChild(removeAmmount)
+
+  // <td>${{ cartItem.price * cartItem.quantity }}</td>
+  const price = document.createElement('td')
+  price.textContent = '$' + cartItem.price * cartItem.quantity
+
+  // <tr> ... </tr>
+  const tr = document.createElement('tr')
+  tr.appendChild(title)
+  tr.appendChild(ammountContainer)
+  tr.appendChild(price)
+
+  return tr
+}
+
+;(async () => {
+  const isAuthenticated = await auth.isAuthenticated()
+  if (!isAuthenticated) location.href = '/sign-in'
+
+  const cart = await cartService.getCart()
+  cart.items
+    .map(renderCartItem)
+    .forEach(element => orderBody.appendChild(element))
+
+  const total = cart.items
+    .map(({ price, quantity }) => price * quantity)
+    .reduce((acc, cur) => acc + cur, 0)
+  totalDataCell.textContent = '$' + total
+})()
